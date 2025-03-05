f3055dc6a07f0022ddc730148c55e378815283e0
Noah Melton <nmelton3@ut.utm.edu>
2025-03-05 13:50:25 -0600

    started on the phonebook assignment

---
+++ phonebook.h
@@ -0,0 +1,32 @@
+/**
+ * @file phonebook.h
+ * @author Noah Melton
+ * @date 2025-03-05
+ * @brief This is the header file for the linked list phonebook assignment. 
+ * 
+ * 
+ */
+
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+class entry {
+
+ private:
+  string first_name;
+  string last_name;
+  string phone_number;
+
+ public:
+  entry();
+
+  
+  
+
+
+
+}
